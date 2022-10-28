73e5183f1a7385c8f2089e707f55056f1a6cd558
Bart Janeczko <janeczko.bartlomiej@gmail.com>
2022-10-28 14:23:40 +0100

    info on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Hello!</h1>
+      <p>
+        This project is build using React, TypeScript and Vite. Check{" "}
+        <Link to="/store">Store</Link> to see all functionalities as adding
+        items to cart, suming their price and increasing/decreasing or removing
+        them. Everything will be saved in localStorage.
+      </p>
+    </>
+  );
 }
